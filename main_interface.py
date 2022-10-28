62424ea976f43e8068c51d6a20650b15dbb4f2e9
melw <98436639+mel-wong@users.noreply.github.com>
2022-10-28 12:49:00 -0400

    Completed scraping of all projected stats and updated main module

--- main_interface.py
+++ main_interface.py
@@ -1,6 +1,8 @@
 # Main module where the fantasy manager's team is defined
 
 import stat_scraper
+import os.path
+import json
 
 # Defines each player
 class Player:
@@ -24,24 +26,28 @@ class Player:
     # Returns the NBA team that the player plays for
     def get_team(self, name):
         #stat_scraper
+        pass
 
 
     # Returns the number of games that week
     def get_weekly_games(self,team):
         #stat_scraper
+        pass
 
     # Populates all stats with both projected and current season stats
     # Called when new player is added
     def get_all_stats(self,name):
+        pass
 
     # Updates current season stats
     def get_curr_season_stats(self,name):
+        pass
 
 
 # Defines the manager's team
 class Team:
 
-    def __init__(self,max_players):
+    def __init__(self,max_players=0):
 
         # Roster is the set of players on manager's team
         self.roster = set()
@@ -62,6 +68,7 @@ class Team:
         if self.can_add_player() == True:
             new_player = Player(name=name)
             self.roster.add(new_player.name)
+            
 
     # Delete player from roster
     def del_player(self,name):
@@ -78,6 +85,100 @@ class Team:
     def update_season_stats(self):
         stat_scraper
 
+
+# Takes in user input. If valid entry, adds player
+def add_player_input(input,team):
+
+
+
+# Main module
+def main():
+
+    print('Initializing Stats...')
+    # Checks if player url list JSON exists
+    if os.path.exists('player_url_list.json'):
+
+        # Already exists so no need to scrape NBA.com
+        with open('player_url_list.json','r') as file:
+            player_url_list = json.load(file)
+
+    # Does not exist so scrape for players and matching URLs
+    else:
+        player_url_list = {}
+        dr = stat_scraper.start_session()
+        stat_scraper.initialize_player_list(dr,player_url_list)
+
+    # Checks if projected stats JSON exists
+    if os.path.exists('all_projected_stats.json'):
+
+        # Already exists so don't need to scrape Rotowire for each player's stats
+        with open('all_projected_stats.json','r') as file:
+            all_projected_stats = json.load(file)
+
+    else:
+        all_projected_stats = {}
+        dr = stat_scraper.start_session()
+        stat_scraper.scrape_all_projected(dr,player_url_list,all_projected_stats)
+
+    print('Done Initialization!')
+
+    # Input the maximum allowable players per team
+    while True:
+        try:
+            max_players = int(input('Max players in a team: '))
+        except ValueError:
+            print('Not a valid number. Try again.')
+        else:
+            break
+
+    # Initialize two teams with the max number of players
+    my_team = Team(max_players=max_players)
+    opp_team = Team(max_players=max_players)
+
+    print('Time to pick your team!')
+
+    # Continuously asks what the user would like to do
+    while True:
+        print('What would you like to do?')
+        try:
+            choice = int(input('1 - Add player to my team, 2 - Remove player from my team, \n '
+              '3 - Add player to opposing team, 4 - Remove player from opposing team, '
+                  '5 - Compare Teams!, 6 - Exit: '))
+        except ValueError:
+            print('Invalid entry')
+        else:
+            if choice < 1 or choice > 6:
+                print('Invalid entry')
+            else:
+
+                if choice == 1:
+
+
+
+                break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 
--- stat_scraper.py
+++ stat_scraper.py
@@ -1,7 +1,5 @@
 # File to scrape Rotowire and NBA.com for player and team statistics
 import time
-
-import requests
 import bs4
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,6 +8,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+import json
+import os.path
 
 
 # Create browser session to access different stat websites
@@ -18,18 +18,18 @@ def start_session():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('window-size=1920x1080')
-    driver = webdriver.Chrome(service = service,options=chrome_options)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     return driver
 
 
-# get full list of NBA players from NBA.com
-def get_player_list(driver,player_list):
+# write player names and Rotowire URL dictionary to JSON
+def initialize_player_list(driver, player_list):
 
     driver.get('https://www.nba.com/players')
     dropdown = 'div.Pagination_pageDropdown__KgjBU > div > label > div>select'
-    dropdown = Select(driver.find_element(by=By.CSS_SELECTOR,value=dropdown))
-    dropdown.select_by_index(0)
+    dropdown = Select(driver.find_element(by=By.CSS_SELECTOR, value=dropdown))
+    dropdown.select_by_index(1)
     page_source = driver.page_source
     soup = bs4.BeautifulSoup(page_source, "lxml")
     table = soup.select("table[class=players-list]")
@@ -41,61 +41,117 @@ def get_player_list(driver,player_list):
         # Ignore header row
         for row in rows[1:]:
             name = row.select('p')
-            player_list.append(name[0].text + ' ' + name[1].text)
+            key = name[0].text + ' ' + name[1].text
 
+            # set URL to None for now
+            player_list[key] = None
 
+    # Populate player list with corresponding Rotowire URL
+    for k in player_list.keys():
+        url = scrape_player_url(driver, k)
+        player_list[k] = url
 
-# get access to Rotowire
-def get_page_access(browser):
+        if url is None:
+            print(f'Cannot find {k}')
 
-    # check if successfully accessed Rotowire
-    if browser.page.status_code == requests.codes.ok:
-        print('success!')
-        page_soup = BeautifulSoup(self.html,'lxml')
-        return page_soup
-    else:
-        return None
 
 # get player's projected stats from Roto
-def scrape_projected(driver,player):
+def scrape_player_url(driver, player):
 
     # Checks if already on Rotowire site
     if driver.current_url is not None and 'rotowire' in driver.current_url:
         print('Already on rotowire')
     else:
         driver.get('https://www.rotowire.com')
-        search_css = "#search-for-players"
-        search_xpath = '/html/body/div/div/header/div[1]/div[2]/div[3]/div[1]/input'
-        #input_element = driver.find_element(by=By.CSS_SELECTOR,value=search_css)
-        input_element = driver.find_element(by=By.XPATH, value=search_xpath)
-        actions = ActionChains(driver)
-        actions.move_to_element(input_element)
-        actions.send_keys(player)
-        actions.send_keys(Keys.ENTER)
-        actions.perform()
-        #driver.execute_script('arguments[0].send_keys(player)',input_element)
-        #input_element.click()
-        #input_element.send_keys(player)
-       # input_element.send_keys(Keys.ENTER)
 
-        time.sleep(3)
+    # Automate searching for player on Rotowire to get their page URL for stats
+    search_css = "#search-for-players"
+    input_element = driver.find_element(by=By.CSS_SELECTOR, value=search_css)
+    actions = ActionChains(driver)
+    actions.click(input_element)
+    actions.send_keys(player)
+    actions.send_keys(Keys.ENTER)
+    actions.perform()
 
-        print(driver.current_url)
+    # Fixes issue with slightly differing names between Rotowire and NBA.com
+    while 'search' in driver.current_url:
+
+        # Accounts for periods in names
+        if '.' in player:
+            split_name = player.split('.')
+            rev_name = ''.join(split_name)
+            scrape_player_url(driver, rev_name)
+
+        # Ignore third string which usually consists of Sr, Jr, I
+        elif ' ' in player:
+            split_name = player.split(' ')
+            split_name.pop()
+            rev_name = ' '.join(split_name)
+            scrape_player_url(driver, rev_name)
+
+        # Cannot find player
+        else:
+            break
+
+    else:
+        return driver.current_url
 
 
+# Scrape the player's projected stats
+def scrape_player_projected(driver, player_list, player):
+
+    url = player_list[player]
+    driver.get(url)
+
+    # Give page time to load stats before scraping
+    time.sleep(1)
+    page_source = driver.page_source
+    soup = bs4.BeautifulSoup(page_source, "lxml")
+    table = soup.select("div.webix_ss_footer > div.webix_hs_center > table>tbody")
+    stat_list = []
+    print(player)
+
+    for t in table:
+
+        rows = t.findAll('tr')
+
+        # Ignore header row
+        for row in rows[1:]:
+            cols = row.findAll('td')
+
+            # Ignore age column
+            for col in cols[1:]:
+                stat = col.find('div')
+
+                # Convert to float
+                stat = float(stat.text)
+                stat_list.append(stat)
+
+    return stat_list
 
 
-    search_css = "div[=nbaStats] > div[id=webix_hs_center]> table"
+# Scrape projected stats for all players and save as JSON
+def scrape_all_projected(driver, player_list, all_projected_stats):
 
+    for player in player_list.keys():
+        all_projected_stats[player] = scrape_player_projected(driver, player_list, player)
 
-
-
+    # Write all data to json file
+    filename = 'all_projected_stats.json'
+    out_file = open(filename, 'w')
+    json.dump(all_projected_stats, out_file)
+    out_file.close()
 
 
 if __name__ == '__main__':
 
-    player_list = []
+    if os.path.exists('player_url_list.json'):
+
+        # Load JSON as dictionary
+        with open('player_url_list.json', 'r') as file:
+            player_list = json.load(file)
+
+    # player_list = {}
     dr = start_session()
-    scrape_projected(dr,'siakam')
-    #get_player_list(dr,player_list)
-    #print(player_list[0])
+    all_projected_stats = {}
+    scrape_all_projected(dr, player_list, all_projected_stats)
